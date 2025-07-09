b0892a01d6da8795f6cef6c3af8c901d472b706d
Tarun <mallangitarun@hotmail.com>
2025-07-09 22:53:03 +0530

    Added update functionality

--- task-cli.py
+++ task-cli.py
@@ -26,31 +26,43 @@ class Task:
     
     @property
     def createdAt(self):
-        return self.__createdAt
+        return self.__createdAt.strftime('%a %d %b %Y, %I:%M%p')
     
     @property
     def updatedAt(self):
-        return self.__updatedAt
+        return self.__updatedAt.strftime('%a %d %b %Y, %I:%M%p')
+    
+    def update(self, new_description):
+        self.__description = new_description
+        self.__updatedAt = datetime.now()
     
     def __str__(self):
         return f"{self.__id}: {self.__description} - {self.__status}"
 
 def add_task(task_list, description):
     new_task = Task(description)
-    task_list.append(new_task)
+    task_list[new_task.id] = new_task
     print(f"Task added successfully (ID: {new_task.id})")
 
 def list_tasks(task_list, status=None):
     if status:
-        for task in task_list:
+        for task in task_list.values():
             if task.status == status:
                 print(task)
     else:
-        for task in task_list:
+        for task in task_list.values():
             print(task)
 
+def update_task(task_list, id, new_description):
+    try:
+        task_list[int(id)].update(new_description)
+    except KeyError:
+        logging.error(f"Task with id: {id} does not exist.")
+    except ValueError:
+        logging.error(f"Incorrect index. Index must be an integer.")
+
 def start_application():
-    task_list = []
+    task_list = {}
     while True:
         command = input("task-cli ")
         parts = command.split()        
@@ -64,10 +76,18 @@ def start_application():
                 list_tasks(task_list, parts[1])
             except IndexError:
                 list_tasks(task_list)
-        
+        elif parts[0] == "update":
+            try:
+                update_task(task_list, parts[1], parts[2])
+            except IndexError:
+                logging.error("Incorrect input. Please input ID and description.")
 
 
 def main():
+    logging.basicConfig(
+        format="%(levelname)s - %(message)s",
+        style="%"
+    )
     start_application()
 
 main()
