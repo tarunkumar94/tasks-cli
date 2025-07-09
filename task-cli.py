04930c21bee7869999d4a29fc98791096b8d55f1
Tarun <mallangitarun@hotmail.com>
2025-07-09 22:22:15 +0530

    Initial commit: Added add task functionality

---
+++ task-cli.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+import itertools
+import logging
+
+class Task:
+    id_iter = itertools.count(start=1)
+
+    def __init__(self, description):
+        self.__id = next(Task.id_iter)
+        self.__description = description
+        self.__status = "todo"
+        self.__createdAt = datetime.now()
+        self.__updatedAt = self.__createdAt
+
+    @property
+    def id(self):
+        return self.__id
+    
+    @property
+    def description(self):
+        return self.__description
+    
+    @property
+    def status(self):
+        return self.__status
+    
+    @property
+    def createdAt(self):
+        return self.__createdAt
+    
+    @property
+    def updatedAt(self):
+        return self.__updatedAt
+    
+
+def add_task(task_list, description):
+    new_task = Task(description)
+    task_list.append(new_task)
+    print(f"Task added successfully (ID: {new_task.id})")
+
+def start_application():
+    task_list = []
+    while True:
+        command = input("task-cli ")
+        parts = command.split()        
+        if parts[0] == "add":
+            try:
+                add_task(task_list, parts[1])
+            except IndexError :
+                logging.error("No description")
+        
+
+
+def main():
+    start_application()
+
+main()
